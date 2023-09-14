b23b983897cc2e6ffe54a0aa9b16c07b9afdb00f
Selda Larsson <selda.larssonsvensdotter@elev.ga.ntig.se>
2023-09-14 10:58:37 +0200

    inte klar än

--- Quiz_prog/Program.cs
+++ Quiz_prog/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Hello and Welcome to my tre question quize");
+String 1a = "Link"
+String 1b = ""
+string q1a = 1a + 1b + 1c;
+string q1 = "What is the name of the hero in Zelda?" + q1a;
+
+if q1a = 1a {
+
+}
+
+int sum = 24 + 8;
+
+Console.WriteLine($"Summan är {sum}!");
+
+Console.ReadLine();
